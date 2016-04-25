faa7bba61a8c57d87efa8ad9f8c920c3a1b52631
hugoeflavia <hugo.mendessp@yahoo.com.br>
2016-04-24 23:11:31 -0300

    só aprimorei algumas coisas para mostrar quem ganhou

--- tabuleiro.py
+++ tabuleiro.py
@@ -1,6 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 23 20:26:11 2016
+
+@author: Hugo
+"""
+
 import tkinter as tk
 from gameplay import *
-
+import time
 
 class tabuleiro:
     def __init__(self,height,width,row,column):
@@ -10,38 +17,40 @@ class tabuleiro:
         self.column = column
     
     def criar_tabuleiro(botão):
+        printar = tk.Label(window)
         botãox = tk.Button(window)
         botãox.config( height = 10,width = 20)
         botãox.grid(row = botão.row,column = botão.column)
         botãox.configure(bg = "white")
         botõesx.append(botãox)
         botãox.configure(command= lambda: tabuleiro.rodar(botão.row,botão.column,jogo.turno))
+        a = Base.verifica_ganhador()
         if jogo.matriz[botão.row][botão.column] == -1:
             botãox.configure(text="X")
+            if a == 1:
+                printar.configure(text="Jogador X ganhou!")
+                printar.grid(row=1,column=1)
+                tabuleiro.resetar_tabuleiro()
+            elif a == 0:
+                printar.configure(text="DEU VELHA")
+                printar.grid(row=1,column=1)
            
         elif jogo.matriz[botão.row][botão.column]== 1:
             botãox.configure(text="O")
+            if a == 2:
+                printar.configure(text="Jogador O ganhou!")
+                printar.grid(row=1,column=1)
+            elif a == 0:
+                printar.configure(text="DEU VELHA")
+                printar.grid(row=1,column=1)
+            
             
     def rodar(i,n,c):
         Base.recebe_jogada(i,n)
-        printar = tk.Label(window)
         a = Base.verifica_ganhador()
         if not a == -1:
-#            
-            if a == 0:
-                printar.configure(text=" DEU VELHA")
-                printar.grid(row=1,column=1)
-            elif a == 1:
-                for p in botões:
-                    tabuleiro.criar_tabuleiro(p)
-                printar.configure(text=" Jogador X ganhou")
-                printar.grid(row=1,column=1)
-                
-              
-            else:
-                printar.configure(text="Jogador Y ganhou")
-                printar.grid(row=1,column=1)
-       
+            for p in botões:
+                tabuleiro.criar_tabuleiro(p)
         if a == -1:
             for p in botões:
                 tabuleiro.criar_tabuleiro(p)
@@ -52,9 +61,13 @@ class tabuleiro:
             elif c == 2:
                 label.configure(text="VEZ DO JOGADOR X")
                 label.grid(row=3,column=1)
-     
-        window.mainloop()
-
+    
+    def resetar_tabuleiro():
+        Base.limpar_jogadas()
+        for p in botões:
+                tabuleiro.criar_tabuleiro(p)
+        
+        
 window = tk.Tk() 
 label =tk.Label(window)       
 
